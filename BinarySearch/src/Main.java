81aa245dade070106c8ae0166a353d0edf53e007
vlaislav <34138781+vlaislav@users.noreply.github.com>
2022-09-28 11:25:06 +0200

    Update Main.java

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+public class BinarySearch {
     public static void main(String[] args) {
-        int[] sortedArray = {19, 20, 26, 48, 55, 64, 75, 89};
-        System.out.println("Hello world!");
+        int[] sortedArray = {16, 52, 44, 12, 1, 67, 32, 11, 5};
+        int key = 89;
+        int position = binarySearch(sortedArray, key);
+        System.out.println(key + " position is " + position);
+    }
+
+    static int binarySearch(int[] array, int key){
+        int position = 0;
+        int left = 0;
+        int right = array.length-1;
+
+
+        while(left <= right){
+            int middle = (left + right)/2;
+            if(array[middle] == key){
+                return middle;
+            }
+
+            if(array[middle] < key){
+                left = middle + 1;
+            }
+
+            if(array[middle] > key){
+                right = middle - 1;
+            }
+        }
+
+        return -1;
     }
 }
